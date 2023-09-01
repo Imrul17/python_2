6a9a49f65b3c0f49258292a663d04d5608943f67
Md Imrul Mia <143766740+Imrul17@users.noreply.github.com>
2023-09-01 19:27:24 +0600

    Add files via upload

---
+++ variable_3.py
@@ -0,0 +1,23 @@
+
+x=10
+print(x)
+x=20
+# will you print?
+#this ans 10 values
+
+y=50
+y=80
+print(y)
+#will your print??
+#this ans 80 values
+
+number= 50
+print(type(number))
+number=5.5
+print(type(number))
+name="imrul kayes"
+print(type(name))
+number=1654549878965478.64864
+print(type(number))
+number=165454987896547864864
+print(type(number))
