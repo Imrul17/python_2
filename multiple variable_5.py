7b8b1c3c71290743c6ccafb718c3f0b2834cac5b
Md Imrul Mia <143766740+Imrul17@users.noreply.github.com>
2023-09-01 19:56:27 +0600

    Add files via upload

---
+++ multiple variable_5.py
@@ -0,0 +1,27 @@
+x=y=z="imrul kayes"
+print(x,y,z)
+#same output
+#just line defferint
+
+x=y=z="imrul kayes"
+print(x)
+print(y)
+print(z)
+
+# multiple velues
+fruits=["apple","banana","cherry"]
+
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
+
+number=["apple",500,40.50]
+
+x,y,z = number
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
