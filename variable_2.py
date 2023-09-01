8aa7edac6cea805c2a87ff43a3caf70a87e0d0f5
Md Imrul Mia <143766740+Imrul17@users.noreply.github.com>
2023-09-01 19:07:53 +0600

    Add files via upload

---
+++ variable_2.py
@@ -0,0 +1,10 @@
+
+Name=str(input("Enter your Name :"))
+Age=int(input("Enter your Age :"))
+GPA=float(input("Enter your GPA :"))
+Home_town=str(input("Enter your Home_town :"))
+
+print("Name :"+Name,"\n","Age :"+str(Age),"\n","GPA :"+str(GPA),"\n","Home_town :"+Home_town)
+
+print( "Hello Everyone \n My name is" +Name, "I study for being well educated. I am study for at Chapainawabgonj Polytechnic Inistitute."
+        "\n I am "+str(Age), "years old. My Department name is Computer Science Technology. And my First semester result " + str(GPA), " \n My Home_town name is "+Home_town)
