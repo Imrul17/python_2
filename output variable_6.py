71d44bee4bd523d739a5c8e2397569c30baf5d2f
Md Imrul Mia <143766740+Imrul17@users.noreply.github.com>
2023-09-01 21:37:20 +0600

    Add files via upload

---
+++ output variable_6.py
@@ -0,0 +1,22 @@
+
+# output variable
+x="Awesome"
+print("python is "+x)
+
+x,y,z="really"," really"," good"
+print("python is "+x+y+z)
+
+#again variable
+
+x = "Python is most "
+y = "popular language "
+z = "all over the World"
+a = x+y+z
+print(a)
+print(x+y+z)
+
+#int and str add. its not possible.but its possible
+# class string
+number =str(50)
+name = " imrul kayes"
+print(number+ name)
