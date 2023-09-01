2beebf2bb046cdb3a0e7e36bbd85078750e0f286
Md Imrul Mia <143766740+Imrul17@users.noreply.github.com>
2023-09-01 19:41:36 +0600

    Add files via upload

---
+++ multiple variable_4.py
@@ -0,0 +1,23 @@
+x,y,z=10,"imrul kayes",3.50
+print(x, y,z)
+#same output
+x,y,z=10,"imrul kayes",3.50
+print(x,"\n", y,"\n",z)
+#same outout
+x,y,z=10,"imrul kayes",3.50
+print(x)
+print(y)
+print(z)
+# same output
+
+x,y,z="hasan","imrul kayes","nafi"
+print(x)
+print(y)
+print(z)
+# same output
+
+x,y,z="hasan","imrul kayes","nafi"
+print(x)
+print(y)
+print(z)
+# same output
